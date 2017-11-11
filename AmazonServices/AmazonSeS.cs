0f68261e4fc2f38e2a10e9e93031f72e3006a735
Gustavo Fonseca De Almeida <guga.fonseca@hotmail.com>
2017-11-10 22:40:40 -0200

    Add project files.

---
+++ AmazonServices/AmazonSeS.cs
@@ -0,0 +1,70 @@
+﻿using Amazon;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AmazonServices
+{
+    public class AmazonSeS
+    {
+        public string Subject { get; set; }
+        public string Para { get; set; }
+        public MailMessage Message { get; set; }
+        public string Template { get; set; }
+        public string Categoria { get; set; }
+
+
+        public AmazonSeS(string para)
+        {
+            Para = para;
+            Message = new MailMessage();
+            Message.From = new MailAddress("soubotafogo@bfr.com.br", "Botafogo de Futebol e Regatas");
+            Message.To.Add(Para);
+        }
+        
+
+        private static MemoryStream ConvertMailMessageToMemoryStream(MailMessage message)
+        {
+            Assembly systemAssembly = typeof(SmtpClient).Assembly;
+            Type mailWriterType = systemAssembly.GetType("System.Net.Mail.MailWriter");
+            const BindingFlags nonPublicInstance = BindingFlags.Instance | BindingFlags.NonPublic;
+            ConstructorInfo mailWriterContructor = mailWriterType.GetConstructor(nonPublicInstance, null, new[] { typeof(Stream) }, null);
+
+            MethodInfo sendMethod = typeof(MailMessage).GetMethod("Send", nonPublicInstance);
+            MethodInfo closeMethod = mailWriterType.GetMethod("Close", nonPublicInstance);
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                object mailWriter = mailWriterContructor.Invoke(new object[] { memoryStream });
+                //AssertHelper.IsTrue(sendMethod.GetParameters().Length > 2, ".NET framework must be 4.5 or higher in order to properly encode email subject.");
+                sendMethod.Invoke(message, nonPublicInstance, null,
+                    new[] { mailWriter, true, false },
+                    null);
+                closeMethod.Invoke(mailWriter, nonPublicInstance, null, new object[] { }, null);
+                return memoryStream;
+            }
+        }
+
+        public virtual async Task Enviar(string templateId = null)
+        {
+
+            Message.Subject = Subject;
+            Message.IsBodyHtml = true;
+            Message.Body = Template;
+
+            using (var stream = ConvertMailMessageToMemoryStream(Message))
+            using (AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(ConfigurationManager.AppSettings["awsAccessKeyId"], ConfigurationManager.AppSettings["awsSecretAccessKey"].ToString(), RegionEndpoint.USEast1))
+            {
+                var sendRequest = new SendRawEmailRequest { RawMessage = new RawMessage { Data = stream } };
+                await client.SendRawEmailAsync(sendRequest);
+            }
+        }
+    }
+}
---
+++ AmazonServices/Bucket.cs
@@ -0,0 +1,78 @@
+﻿using Amazon.S3;
+using Amazon.S3.Transfer;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AmazonServices
+{
+    public class Bucket
+    {
+        public string BucketDestino { get; set; }
+        public string FileName { get; set; }
+        public StreamReader File { get; set; }
+
+        public Bucket()
+        {
+        }
+
+        public string SaveToBlob(string bucketDestino, MemoryStream filePath, string contentType)
+        {
+            try
+            {
+               
+                TransferUtility fileTransferUtility = new
+                    TransferUtility(new AmazonS3Client(ConfigurationManager.AppSettings["awsAccessKeyId"].ToString(), ConfigurationManager.AppSettings["awsSecretAccessKey"].ToString(), Amazon.RegionEndpoint.SAEast1)
+                    );
+
+
+                string nomeArquivo = Guid.NewGuid().ToString();
+                nomeArquivo = nomeArquivo + "." + contentType;
+                fileTransferUtility.Upload(filePath, bucketDestino, nomeArquivo);
+                var urlBase = @"https://s3-sa-east-1.amazonaws.com/";
+                var urlRetorno = urlBase + bucketDestino + "/" + nomeArquivo;
+                return urlRetorno;
+
+            }
+            catch (AmazonS3Exception s3Exception)
+            {
+                var x = s3Exception.Message;
+                return "Error" + x;
+
+
+            }
+        }
+
+
+        public async Task<String> SaveToBlobAsync(string bucketDestino, MemoryStream filePath, string contentType)
+        {
+            try
+            {
+                TransferUtility fileTransferUtility = new
+                   TransferUtility(new AmazonS3Client(ConfigurationManager.AppSettings["awsAccessKeyId"].ToString(), ConfigurationManager.AppSettings["awsSecretAccessKey"].ToString(), Amazon.RegionEndpoint.SAEast1)
+                   );
+
+                string nomeArquivo = Guid.NewGuid().ToString();
+                nomeArquivo = nomeArquivo + "." + contentType;
+                await fileTransferUtility.UploadAsync(filePath, bucketDestino, nomeArquivo);
+                string url = @"https://s3-sa-east-1.amazonaws.com/";
+                url += bucketDestino + "/" + nomeArquivo;
+                return url;
+            }
+            catch (AmazonS3Exception s3Exception)
+            {
+                var x = s3Exception.Message;
+                return "Error" + x;
+
+
+            }
+        }
+
+
+
+    }
+}
